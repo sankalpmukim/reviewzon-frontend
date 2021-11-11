bf0ddde88d17fa0d429c5ca41f8594140c5798e1
sankalpmukim <sankalpmukim@gmail.com>
2021-11-11 15:57:18 +0530

    Home: Placeholder image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/fontawesome-free-solid";
 import fontawesome from "@fortawesome/fontawesome";
-
+import { ReactComponent as PlaceholderImage } from "../static/Placeholder.svg";
 import Logo from "../logo192.png";
 
 fontawesome.library.add(faChevronRight);
@@ -25,10 +25,18 @@ export const Home = () => {
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             }
           </div>
-          <div className="col-4 text-center">{"<Insert image here>"}</div>
+          <div className="col-4 text-center">
+            <div>
+              <PlaceholderImage className="svg-image" alt="placeholder svg" />
+            </div>
+          </div>
         </div>
         <div className="row p-10">
-          <div className="col-4 text-center">{"<Insert image here>"}</div>
+          <div className="col-4 text-center">
+            <div>
+              <PlaceholderImage className="svg-image" alt="placeholder svg" />
+            </div>
+          </div>
           <div className="col-8">
             {
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
