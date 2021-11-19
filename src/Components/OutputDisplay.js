177104d69b3c3ba9d1eee8c8d86f5a85d5ebc88d
sankalpmukim <sankalpmukim@gmail.com>
2021-11-19 13:34:19 +0530

    Output display

--- src/Components/OutputDisplay.js
+++ src/Components/OutputDisplay.js
@@ -1,6 +1,105 @@
-import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/fontawesome-free-solid";
+import { OutputItem } from "./OutputItem";
+import { getDatabase, ref as dbRef, get } from "firebase/database";
 
 export const OutputDisplay = () => {
   const { uniqueKey } = useParams();
-  return <div>{uniqueKey}</div>;
+  const [output, setOutput] = useState([]);
+  const [staticInfo, setStaticInfo] = useState(null);
+  const [allOutputData, setAllOutputData] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const db = getDatabase();
+    const allOutputRef = dbRef(db, `output/${uniqueKey}`);
+    get(allOutputRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+          setAllOutputData(snapshot.val());
+        } else {
+          console.log(`No data available for this key: ${uniqueKey}`);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [uniqueKey]);
+
+  useEffect(() => {
+    if (allOutputData === null) return;
+    const transformData = (obj) => {
+      const cleanedObj = {};
+      for (const key in obj) {
+        if (key === `static`) continue;
+        cleanedObj[obj[key].counter] = {
+          ...obj[key],
+          title: key,
+        };
+      }
+      let i = 0;
+      const arr = [];
+      console.log(cleanedObj);
+      while (true) {
+        if (typeof cleanedObj[i] === "undefined") break;
+        arr.push({
+          imgSrc: cleanedObj[i].url,
+          title: cleanedObj[i].title,
+          bodyText: cleanedObj[i].data,
+          unique: i,
+        });
+        i++;
+      }
+      return {
+        data: arr,
+        static: obj.static,
+      };
+    };
+    setOutput(transformData(allOutputData).data);
+    setStaticInfo(transformData(allOutputData).static);
+  }, [allOutputData]);
+
+  return (
+    <div className="output-container">
+      <div className="output-header">
+        <div className="back-button">
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              const url = new URL(`http://127.0.0.1/progressdisplay`);
+              url.searchParams.append(`data`, JSON.stringify(staticInfo));
+              navigate(url.pathname + url.search);
+            }}
+            style={{
+              width: "100px",
+              height: "50px",
+              fontSize: "150%",
+            }}
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+        </div>
+        <div className="output-header-content">
+          <h2 className="text-center">{`this is the output display page and this is the unique key: ${uniqueKey}`}</h2>
+        </div>
+      </div>
+      <div className="output-content">
+        <div className="accordion" id="accordionParent">
+          {output.map(({ imgSrc, title, bodyText, unique }) => (
+            <OutputItem
+              imgSrc={imgSrc}
+              title={title}
+              bodyText={bodyText}
+              unique={unique}
+              key={unique}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ src/Components/OutputItem.js
@@ -0,0 +1,51 @@
+import "./CSS/OutputDisplay.css";
+
+export const OutputItem = ({ imgSrc, title, bodyText, unique }) =>
+  imgSrc !== "None" ? (
+    <div className="accordion-item">
+      <h2 className="accordion-header" id={`heading${unique}`}>
+        <button
+          className="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target={`#main${unique}`}
+        >
+          {title}
+        </button>
+      </h2>
+      <div
+        id={`main${unique}`}
+        className="accordion-collapse collapse"
+        data-bs-parent="#accordionParent"
+      >
+        <div className="accordion-body grid-separate">
+          <div className="output-image">
+            <img src={imgSrc} alt={`${title} graph/visualization`} />
+          </div>
+          <div className="output-text">{bodyText}</div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="accordion-item">
+      <h2 className="accordion-header" id={`heading${unique}`}>
+        <button
+          className="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target={`#main${unique}`}
+        >
+          {title}
+        </button>
+      </h2>
+      <div
+        id={`main${unique}`}
+        className="accordion-collapse collapse"
+        data-bs-parent="#accordionParent"
+      >
+        <div className="accordion-body">
+          <div className="output-text">{bodyText}</div>
+        </div>
+      </div>
+    </div>
+  );
