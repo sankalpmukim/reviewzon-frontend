ae06a5daf1b9f2344a50fe9ee4a81554a2118fae
sankalpmukim <sankalpmukim@gmail.com>
2021-11-20 16:42:30 +0530

    Fix: interactive terminal xD

--- src/Components/TerminalPrompt.js
+++ src/Components/TerminalPrompt.js
@@ -33,6 +33,7 @@ export const TerminalPrompt = ({ setPrompt, commands, setContent }) => {
               component: StaticPrompt,
               props: {
                 oldText: commandText,
+                setPrompt: setPrompt,
               },
             },
             commands[commandText],
