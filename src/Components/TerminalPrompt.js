17a9ef31d61d4ff3d367852c97dcddab6072c50c
sankalpmukim <sankalpmukim@gmail.com>
2021-11-24 10:32:01 +0530

    Added better text features+ Fixed scrollbar

--- src/Components/TerminalPrompt.js
+++ src/Components/TerminalPrompt.js
@@ -66,25 +66,46 @@ export const TerminalPrompt = ({ setPrompt, commands, setContent }) => {
     };
     const registerKeyPresses = (e) => {
       const command = e.code;
-      console.log(`${command} pressed`);
-      if (command === "Backspace") {
-        setLiveText((text) => text.substring(0, text.length - 1));
-      } else if (command.length === 4 && command.substring(0, 3) === "Key") {
-        setLiveText((text) => text + command.substring(3).toLowerCase());
-      } else if (command === "Space") {
-        setLiveText((text) => text + " ");
-      } else if (command === "Quote") {
-        setLiveText((text) => text + "'");
-      } else if (command === "Minus") {
-        setLiveText((text) => text + "-");
-      } else if (command.length === 6 && command.substring(0, 5) === "Digit") {
-        setLiveText((text) => text + command.substring(5));
-      } else if (command.length === 7 && command.substring(0, 6) === "Numpad") {
-        setLiveText((text) => text + command.substring(6));
-      } else if (command === "Enter") {
-        onEnterPress(liveText);
-      } else {
-        console.log(`${command.length} && ${command.substring(0, 6)}`);
+      // console.log(`${command} pressed`);
+      if (!e.ctrlKey && !e.altKey && !e.metaKey) {
+        if (command === "Backspace") {
+          setLiveText((text) => text.substring(0, text.length - 1));
+        } else if (command.length === 4 && command.substring(0, 3) === "Key") {
+          if (e.shiftKey) {
+            setLiveText((text) => text + command.substring(3).toUpperCase());
+          } else {
+            setLiveText((text) => text + command.substring(3).toLowerCase());
+          }
+        } else if (command === "Space") {
+          setLiveText((text) => text + " ");
+        } else if (command === "Quote") {
+          setLiveText((text) => text + "'");
+        } else if (command === "Minus") {
+          setLiveText((text) => text + "-");
+        } else if (
+          command.length === 6 &&
+          command.substring(0, 5) === "Digit"
+        ) {
+          setLiveText((text) => text + command.substring(5));
+        } else if (
+          command.length === 7 &&
+          command.substring(0, 6) === "Numpad"
+        ) {
+          setLiveText((text) => text + command.substring(6));
+        } else if (command === "Enter") {
+          onEnterPress(liveText);
+        } else {
+          console.log(`${command.length} && ${command.substring(0, 6)}`);
+        }
+      } else if (e.ctrlKey && e.code === "Backspace") {
+        setLiveText((text) => {
+          const arr = text
+            .trim()
+            .split(" ")
+            .filter((v, i, a) => i !== a.length - 1);
+          const str = arr.join(" ");
+          return str;
+        });
       }
     };
     document.addEventListener("keyup", registerKeyPresses);
